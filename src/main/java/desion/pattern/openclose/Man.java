9e8c2cc3d3ae691f5ab208d66fdb3a4dd398300e
MyBaron <249790774@qq.com>
2019-05-10 09:46:40 +0800

    设计模式--开闭原则

--- src/main/java/desion/pattern/openclose/Man.java
+++ src/main/java/desion/pattern/openclose/Man.java
@@ -2,15 +2,10 @@ package desion.pattern.openclose;
 
 public class Man implements Person {
     private int age;
+
+
     @Override
     public void eat() {
         System.out.println("每天都要吃饭");
-    }
-
-//    如果吃饭前需要添加个洗手的方法，那么不应该在Person里添加，
-//    而是在Man类或者新建一个类继承Man来实现
-//    此处就在Man类中实现
-    public void washinghands() {
-        System.out.println("要洗手");
     }
 }
